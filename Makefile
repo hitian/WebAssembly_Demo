02efeac813a2279cc8cf2fcb53d9d332971bf1da
Jia <jia.tian@me.com>
2019-07-16 12:59:46 +0800

    test  function.

--- Makefile
+++ Makefile
@@ -5,8 +5,8 @@ clean:
 	
 build:
 	#cp "$(go env GOROOT)/misc/wasm/wasm_exec.js" .
-	GOOS=js GOARCH=wasm go build -o main.wasm
+	GOOS=js GOARCH=wasm go build -ldflags="-X main.version=`date +'%Y-%m-%d_%H_%M_%S'`" -o main.wasm
 
 serve:
 	go get github.com/shurcooL/goexec
-	goexec 'http.ListenAndServe(":9000", http.FileServer(http.Dir(".")))'
+	goexec 'http.ListenAndServe("127.0.0.1:9000", http.FileServer(http.Dir(".")))'
--- main.go
+++ main.go
@@ -1,13 +1,80 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"sync"
+	"syscall/js"
+)
+
+var (
+	version = "debug"
 )
 
 func main() {
 	fmt.Println("Hello, WebAssembly!")
+	fmt.Println("version ", version)
 	wg := &sync.WaitGroup{}
 	wg.Add(1)
+
+	registerJsFunctions()
+
 	wg.Wait()
+}
+
+func registerJsFunctions() {
+	//add callback to element.
+	var cb js.Func
+	cb = js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		fmt.Println("button clicked")
+		cb.Release()
+		return nil
+	})
+	js.Global().Get("document").Call("getElementById", "myButton").Call("addEventListener", "click", cb)
+
+	//add global function
+	var testFunctionParams js.Func
+	testFunctionParams = js.FuncOf(printFunctionArgs)
+	js.Global().Set("print_args", testFunctionParams)
+
+	js.Global().Set("add", add())
+}
+
+func printFunctionArgs(this js.Value, args []js.Value) interface{} {
+	fmt.Println(this)
+	fmt.Println(args)
+	return nil
+}
+
+func add() js.Func {
+	return js.FuncOf(func(this js.Value, args []js.Value) (result interface{}) {
+		defer func() {
+			if err := recover(); err != nil {
+				result = createErrorResult(fmt.Errorf("%s", err))
+				return
+			}
+		}()
+		if len(args) != 2 {
+			return createErrorResult(errors.New("wrong args length, need 2"))
+		}
+
+		a, b := args[0].Int(), args[1].Int()
+		val := createResult()
+		val["data"] = a + b
+		result = val
+		return
+	})
+}
+
+func createErrorResult(err error) map[string]interface{} {
+	result := make(map[string]interface{})
+	result["ok"] = false
+	result["error"] = err.Error()
+	return result
+}
+
+func createResult() map[string]interface{} {
+	result := make(map[string]interface{})
+	result["ok"] = true
+	return result
 }
