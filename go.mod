5c969979692e50791ca1ea0dc364f446c0b158da
Jia <t@hitian.me>
2022-06-10 09:18:09 +0800

    migrate to golang-jwt

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/hitian/WebAssembly_Demo
 
 go 1.18
 
-require github.com/dgrijalva/jwt-go v3.2.0+incompatible
+require github.com/golang-jwt/jwt/v4 v4.4.1
--- utils/jwt.go
+++ utils/jwt.go
@@ -5,7 +5,7 @@ import (
 	"errors"
 	"fmt"
 
-	"github.com/dgrijalva/jwt-go"
+	"github.com/golang-jwt/jwt/v4"
 )
 
 func JwtGenerate(secret, content string) (string, error) {
